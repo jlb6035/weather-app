9444bbb3a5e717fc1a995971af96a0587ea74051
jlb6035 <jlb6035@gmail.com>
2021-06-04 15:15:37 -0400

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,7 +1,7 @@
 /* Global Variables */
 //api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather?zip=";
-const key = "625542cb6c623287c968f09aa72f2b15&units=imperial";
+const key = "";
 
 const generateButton = document.querySelector("#generate");
 
